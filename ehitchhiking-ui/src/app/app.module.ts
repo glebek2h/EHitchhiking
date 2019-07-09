5fbde2735e12fd47490c1c3e728e7b4e734c1a66
Gleb <raul221299@gmail.com>
2019-07-09 17:34:16 +0300

    add 2 module(main screen)

--- ehitchhiking-ui/src/app/app.module.ts
+++ ehitchhiking-ui/src/app/app.module.ts
@@ -1,18 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCheckboxModule, MatSidenavModule} from '@angular/material';
+import {FormsModule} from '@angular/forms';
+import { MenuComponent } from './shared/components/menu/menu.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MainScreenModule} from './main-screen/main-screen.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    FormsModule,
+    MatButtonModule,
+    MainScreenModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
