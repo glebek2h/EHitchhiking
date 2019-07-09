a6ed2e60b8a8b6e080f90fd7a8646bafd2a66acc
AnastasiaHarost <garost99@.mail.ru>
2019-07-09 13:01:41 +0300

    a

---
+++ ehitchhiking-ui/src/main-screen/main-screen.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainScreenComponent } from './main-screen.component';
+
+
+
+@NgModule({
+  declarations: [MainScreenComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class MainScreenModule { }
