8ce5bfc42007767d75c8539ac332a4fa0a635d53
Глеб Казачинский <37902290+glebek2h@users.noreply.github.com>
2019-07-09 10:39:06 +0300

    Revert "init markup"

--- ehitchhiking-ui/src/app/app.module.ts
+++ ehitchhiking-ui/src/app/app.module.ts
@@ -1,29 +1,18 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatCheckboxModule, MatIconModule, MatMenuModule, MatSidenavModule} from '@angular/material';
-import {FormsModule} from '@angular/forms';
-import { MenuComponent } from './shared/components/menu/menu.component';
-import {MatButtonModule} from '@angular/material/button';
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MenuComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatCheckboxModule,
-    MatSidenavModule,
-    FormsModule,
-    MatButtonModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- ehitchhiking-ui/src/app/shared/components/menu/buttons.ts
+++
@@ -1,8 +0,0 @@
-export const BUTTONS: string[] = [
-  'history' ,
-  'saved' ,
-  'active' ,
-  'notifications' ,
-  'blacklist' ,
-  'admin functionality' ,
-];
