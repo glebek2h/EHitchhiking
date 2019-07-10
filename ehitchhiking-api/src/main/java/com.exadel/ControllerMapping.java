94c568e46263fbddf8d4a48126fcb2cb71e3376d
Chernyakov <alexey.chernyakov00@gmail.com>
2019-07-10 10:31:01 +0300

    init api

--- ehitchhiking-api/src/main/java/com.exadel/ControllerMapping.java
+++ ehitchhiking-api/src/main/java/com.exadel/ControllerMapping.java
@@ -3,19 +3,24 @@ package com.exadel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Map;
+
 @Controller
+@RequestMapping("/test")
 public class ControllerMapping {
 
     @GetMapping("/go")
-    public String go(@RequestParam(name="name", required=false, defaultValue="World" ) String name, Model model) {
-        model.addAttribute("name", name);
+    public String go(@RequestParam(required = false) Map<String, String> params, Model model) {
+        model.addAttribute("name", params.get("name"));
+        model.addAttribute("option", params.get("option"));
         return "go";
     }
 
-    @GetMapping("/")
-    public String main(@RequestParam(name="name", required = false, defaultValue = "Main") String name, Model model){
+    @GetMapping("/main")
+    public String main(@RequestParam(required = false) String name, Model model){
         model.addAttribute("name", name);
         return "main";
     }
