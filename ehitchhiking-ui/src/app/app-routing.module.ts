0e0c5a25d00e58937767366eec6a6e88097b673c
Gleb <raul221299@gmail.com>
2019-07-10 18:11:22 +0300

    minor changes

--- ehitchhiking-ui/src/app/app-routing.module.ts
+++ ehitchhiking-ui/src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {MenuComponent} from './shared/components/menu/menu.component';
 import {MainScreenComponentComponent} from './main-screen/main-screen-component/main-screen-component.component';
 
 
 const routes: Routes = [
-  { path: 'menu', component: MenuComponent },
-  { path: 'main-screen', component: MainScreenComponentComponent },
-  { path: '', redirectTo: '/main-screen', pathMatch: 'full' },
+  { path: 'main', component: MainScreenComponentComponent },
+  { path: '', redirectTo: '/main', pathMatch: 'full' },
 ];
 
 @NgModule({
