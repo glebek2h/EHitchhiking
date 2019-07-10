fd78f95ccde6c018ea0a0eca976e53c2cd34c906
Gleb <raul221299@gmail.com>
2019-07-10 12:53:50 +0300

    add routing, issues fixes

--- ehitchhiking-ui/src/app/app-routing.module.ts
+++ ehitchhiking-ui/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MenuComponent} from './shared/components/menu/menu.component';
+import {MainScreenComponentComponent} from './main-screen/main-screen-component/main-screen-component.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'menu', component: MenuComponent },
+  { path: 'main-screen', component: MainScreenComponentComponent },
+  { path: '', redirectTo: '/main-screen', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
