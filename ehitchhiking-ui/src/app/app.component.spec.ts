bbaf07105951c5fbcee4218884698a7c893fd419
yana <yana.bernachkaya@gmail.com>
2019-07-11 16:51:58 +0300

    fixed isses-2

--- ehitchhiking-ui/src/app/app.component.spec.ts
+++ ehitchhiking-ui/src/app/app.component.spec.ts
@@ -1,33 +1,33 @@
-import {TestBed, async} from '@angular/core/testing'
-import {RouterTestingModule} from '@angular/router/testing'
-import {AppComponent} from './app.component'
+import {TestBed, async} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AppComponent} from './app.component';
 
 describe('AppComponent', () => {
-    beforeEach(async(() => {
-        TestBed.configureTestingModule({
-            imports: [RouterTestingModule],
-            declarations: [AppComponent],
-        }).compileComponents()
-    }))
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			imports: [RouterTestingModule],
+			declarations: [AppComponent],
+		}).compileComponents();
+	}));
 
-    it('should create the app', () => {
-        const fixture = TestBed.createComponent(AppComponent)
-        const app = fixture.debugElement.componentInstance
-        expect(app).toBeTruthy()
-    })
+	it('should create the app', () => {
+		const fixture = TestBed.createComponent(AppComponent);
+		const app = fixture.debugElement.componentInstance;
+		expect(app).toBeTruthy();
+	});
 
-    it(`should have as title 'ehitchhiking-ui'`, () => {
-        const fixture = TestBed.createComponent(AppComponent)
-        const app = fixture.debugElement.componentInstance
-        expect(app.title).toEqual('ehitchhiking-ui')
-    })
+	it(`should have as title 'ehitchhiking-ui'`, () => {
+		const fixture = TestBed.createComponent(AppComponent);
+		const app = fixture.debugElement.componentInstance;
+		expect(app.title).toEqual('ehitchhiking-ui');
+	});
 
-    it('should render title in a h1 tag', () => {
-        const fixture = TestBed.createComponent(AppComponent)
-        fixture.detectChanges()
-        const compiled = fixture.debugElement.nativeElement
-        expect(compiled.querySelector('h1').textContent).toContain(
-            'Welcome to ehitchhiking-ui!'
-        )
-    })
-})
+	it('should render title in a h1 tag', () => {
+		const fixture = TestBed.createComponent(AppComponent);
+		fixture.detectChanges();
+		const compiled = fixture.debugElement.nativeElement;
+		expect(compiled.querySelector('h1').textContent).toContain(
+			'Welcome to ehitchhiking-ui!'
+		);
+	});
+});
--- ehitchhiking-ui/src/app/main-screen/main-screen-component/main-screen.component.spec.ts
+++ ehitchhiking-ui/src/app/main-screen/main-screen-component/main-screen.component.spec.ts
@@ -1,24 +1,24 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing'
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import {MainScreenComponentComponent} from './main-screen-component.component'
+import {MainScreenComponentComponent} from './main-screen-component.component';
 
 describe('MainScreenComponentComponent', () => {
-    let component: MainScreenComponentComponent
-    let fixture: ComponentFixture<MainScreenComponentComponent>
+	let component: MainScreenComponentComponent;
+	let fixture: ComponentFixture<MainScreenComponentComponent>;
 
-    beforeEach(async(() => {
-        TestBed.configureTestingModule({
-            declarations: [MainScreenComponentComponent],
-        }).compileComponents()
-    }))
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [MainScreenComponentComponent],
+		}).compileComponents();
+	}));
 
-    beforeEach(() => {
-        fixture = TestBed.createComponent(MainScreenComponentComponent)
-        component = fixture.componentInstance
-        fixture.detectChanges()
-    })
+	beforeEach(() => {
+		fixture = TestBed.createComponent(MainScreenComponentComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-    it('should create', () => {
-        expect(component).toBeTruthy()
-    })
-})
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+});
