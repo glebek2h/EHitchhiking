0692cb1b1aa68330cee6d9358341e2ee8b129940
Gleb <raul221299@gmail.com>
2019-07-15 12:41:45 +0300

    issues fixes v1

--- ehitchhiking-ui/src/app/pages/main-screen/main-screen.module.ts
+++ ehitchhiking-ui/src/app/pages/main-screen/main-screen.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MainScreenComponent} from './main-screen-component/main-screen.component';
 import {MatButtonModule, MatButtonToggleModule} from '@angular/material';
-import { YandexMapComponent } from './yandex-map/yandex-map.component';
+import {YandexMapComponent} from './yandex-map/yandex-map.component';
 
 @NgModule({
 	declarations: [MainScreenComponent, YandexMapComponent],
--- ehitchhiking-ui/src/app/pages/main-screen/yandex-map/yandex-map.component.ts
+++ ehitchhiking-ui/src/app/pages/main-screen/yandex-map/yandex-map.component.ts
@@ -7,17 +7,18 @@ import ymaps from 'ymaps';
 	styleUrls: ['./yandex-map.component.sass'],
 })
 export class YandexMapComponent implements OnInit {
-
 	constructor() {}
 	ngOnInit() {
-    ymaps
-      .load('https://api-maps.yandex.ru/2.1/?apikey=05c4e476-2248-4d27-836c-4a6c7c45e485&lang=ru_RU')
-      .then(maps => {
-        const map = new maps.Map('map', {
-          center: [-8.369326, 115.166023],
-          zoom: 7
-        });
-      })
-      .catch(error => console.log('Failed to load Yandex Maps', error));
+		ymaps
+			.load(
+				'https://api-maps.yandex.ru/2.1/?apikey=05c4e476-2248-4d27-836c-4a6c7c45e485&lang=ru_RU'
+			)
+			.then((maps) => {
+				const map = new maps.Map('map', {
+					center: [-8.369326, 115.166023],
+					zoom: 7,
+				});
+			})
+			.catch((error) => console.log('Failed to load Yandex Maps', error));
 	}
 }
