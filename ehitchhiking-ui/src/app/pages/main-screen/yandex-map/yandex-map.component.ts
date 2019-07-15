019b6d25d44c7bab9b8b5d464f99e6b20692cbfd
Gleb <raul221299@gmail.com>
2019-07-15 11:52:31 +0300

    yandex-map-component!

--- ehitchhiking-ui/src/app/pages/main-screen/yandex-map/yandex-map.component.ts
+++ ehitchhiking-ui/src/app/pages/main-screen/yandex-map/yandex-map.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import ymaps from 'ymaps';
-(window as any).global = window;
+
 @Component({
 	selector: 'yandex-map',
 	templateUrl: './yandex-map.component.html',
@@ -9,10 +9,9 @@ import ymaps from 'ymaps';
 export class YandexMapComponent implements OnInit {
 
 	constructor() {}
-
 	ngOnInit() {
     ymaps
-      .load()
+      .load('https://api-maps.yandex.ru/2.1/?apikey=05c4e476-2248-4d27-836c-4a6c7c45e485&lang=ru_RU')
       .then(maps => {
         const map = new maps.Map('map', {
           center: [-8.369326, 115.166023],
