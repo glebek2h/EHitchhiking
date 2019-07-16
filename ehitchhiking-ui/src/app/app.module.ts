9e70fead2883fd5fc86acb4873193ffb5680996d
AnastasiaHarost <garost99@.mail.ru>
2019-07-16 10:45:12 +0300

    some changes

--- ehitchhiking-ui/src/app/app.module.ts
+++ ehitchhiking-ui/src/app/app.module.ts
@@ -7,18 +7,21 @@ import {FormsModule} from '@angular/forms';
 import {MainScreenModule} from './pages/main-screen/main-screen.module';
 import {MenuModule} from './shared/components/menu/menu.module';
 import {RatePassengersModalModule} from './shared/components/rate-passengers-modal/rate-passengers-modal.module';
+import { PreLoadingComponent } from './shared/components/pre-loading/pre-loading.component';
+import {MatProgressSpinnerModule} from "@angular/material";
 
 @NgModule({
-	declarations: [AppComponent],
-	imports: [
-		BrowserModule,
-		AppRoutingModule,
-		BrowserAnimationsModule,
-		FormsModule,
-		MainScreenModule,
-		MenuModule,
-		RatePassengersModalModule,
-	],
+	declarations: [AppComponent, PreLoadingComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MainScreenModule,
+    MenuModule,
+    RatePassengersModalModule,
+    MatProgressSpinnerModule,
+  ],
 	providers: [],
 	bootstrap: [AppComponent],
 })
---
+++ ehitchhiking-ui/src/app/shared/components/pre-loading/pre-loading.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pre-loading',
+  templateUrl: './pre-loading.component.html',
+  styleUrls: ['./pre-loading.component.sass']
+})
+export class PreLoadingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
