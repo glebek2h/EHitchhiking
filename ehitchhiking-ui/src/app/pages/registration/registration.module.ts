4425edef99cf280baec3df583fb012d43829a362
Julia <youliaja.romanova@gmail.com>
2019-07-17 12:18:44 +0300

    Prettier

--- ehitchhiking-ui/src/app/pages/registration/registration.module.ts
+++ ehitchhiking-ui/src/app/pages/registration/registration.module.ts
@@ -1,14 +1,12 @@
 import {NgModule} from '@angular/core';
-import {RegistrationComponent} from "./registration.component";
-import {MatButtonModule} from "@angular/material";
-import {FormsModule} from "@angular/forms";
+import {RegistrationComponent} from './registration.component';
+import {MatButtonModule} from '@angular/material';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
-  declarations: [RegistrationComponent],
-  imports: [
-    MatButtonModule,
-    FormsModule,],
-  entryComponents: [RegistrationComponent],
-  exports: [RegistrationComponent],
+	declarations: [RegistrationComponent],
+	imports: [MatButtonModule, FormsModule],
+	entryComponents: [RegistrationComponent],
+	exports: [RegistrationComponent],
 })
 export class RegistrationModule {}
