600ce10c0464d33389fd384e7745c4ce02f6675c
Julia <youliaja.romanova@gmail.com>
2019-07-18 17:15:23 +0300

    Merge branch 'release-1507-2107' into juliya/feature/release-1507-2107/stored-favorite-trips
    Add component trip.

---
+++ ehitchhiking-ui/src/app/shared/components/trip/trip.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { Trip } from "../trips/trips";
+
+@Component({
+  selector: 'app-trip',
+  templateUrl: './trip.component.html',
+  styleUrls: ['./trip.component.sass']
+})
+export class TripComponent implements OnInit {
+
+  @Input() trip: Trip;
+
+  @Output() change: EventEmitter<Trip>;
+
+  constructor() {
+    this.change = new EventEmitter();
+  }
+
+  ngOnInit() {
+  }
+
+  handleChange(mode: string) {
+    switch (mode) {
+      case 'favorite':
+        this.trip.isFavorite = !this.trip.isFavorite;
+        break;
+      default: break;
+    }
+    this.change.emit(this.trip);
+  }
+
+}
