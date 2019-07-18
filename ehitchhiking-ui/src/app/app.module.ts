150cb83bf3cc43d934cf31b718bcd82fed4671a9
AnastasiaHarost <garost99@.mail.ru>
2019-07-18 13:47:29 +0300

    commit

--- ehitchhiking-ui/src/app/app.module.ts
+++ ehitchhiking-ui/src/app/app.module.ts
@@ -7,7 +7,7 @@ import {FormsModule} from '@angular/forms';
 import {MainScreenModule} from './pages/main-screen/main-screen.module';
 import {MenuModule} from './shared/components/menu/menu.module';
 import {RatePassengersModalModule} from './shared/components/rate-passengers-modal/rate-passengers-modal.module';
-import { RegistrationModule } from "./pages/registration/registration.module";
+import { RegistrationModule } from './pages/registration/registration.module';
 
 @NgModule({
 	declarations: [AppComponent],
--- ehitchhiking-ui/src/app/shared/components/menu/menu.module.ts
+++ ehitchhiking-ui/src/app/shared/components/menu/menu.module.ts
@@ -2,28 +2,32 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MenuComponent} from './menu.component';
 import {
-	MatButtonModule,
-	MatCheckboxModule,
-	MatDialogModule,
-	MatSidenavModule,
-	MatTableModule,
+  MatButtonModule, MatCardModule,
+  MatCheckboxModule,
+  MatDialogModule,
+  MatSidenavModule,
+  MatTableModule,
 } from '@angular/material';
 import {BlacklistComponent} from '../blacklist/blacklist.component';
 import {FormsModule} from '@angular/forms';
-import {PreLoadingComponent} from '../pre-loading/pre-loading.component';
+import {PreLoadingModule} from '../pre-loading/pre-loading.module';
+import {NoDataModule} from '../no-data/no-data.module';
 
 @NgModule({
-	declarations: [MenuComponent, BlacklistComponent, PreLoadingComponent],
-	imports: [
-		CommonModule,
-		MatCheckboxModule,
-		MatSidenavModule,
-		FormsModule,
-		MatButtonModule,
-		MatTableModule,
-		MatDialogModule,
-	],
+	declarations: [MenuComponent, BlacklistComponent],
+  imports: [
+    CommonModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    FormsModule,
+    MatButtonModule,
+    MatTableModule,
+    MatDialogModule,
+    PreLoadingModule,
+    NoDataModule,
+    MatCardModule,
+  ],
 	entryComponents: [BlacklistComponent],
-	exports: [MenuComponent, BlacklistComponent, PreLoadingComponent],
+	exports: [MenuComponent, BlacklistComponent],
 })
 export class MenuModule {}
