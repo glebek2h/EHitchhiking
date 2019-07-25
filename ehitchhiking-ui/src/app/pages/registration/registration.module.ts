600324919a3543ee467cbd196da09479c3363870
yana <yana.bernachkaya@gmail.com>
2019-07-25 14:14:20 +0300

    redisign registrayion form

--- ehitchhiking-ui/src/app/pages/registration/registration.module.ts
+++ ehitchhiking-ui/src/app/pages/registration/registration.module.ts
@@ -1,13 +1,21 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {RegistrationComponent} from './registration.component';
-import {MatButtonModule} from '@angular/material';
+import {MatButtonModule, MatInputModule, MatIconModule} from '@angular/material';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
 	declarations: [RegistrationComponent],
-	imports: [MatButtonModule, FormsModule, ReactiveFormsModule, MatFormFieldModule, CommonModule],
+	imports: [
+		MatButtonModule,
+		MatInputModule,
+		FormsModule,
+		ReactiveFormsModule,
+		MatFormFieldModule,
+		CommonModule,
+		MatIconModule,
+	],
 	entryComponents: [RegistrationComponent],
 	exports: [RegistrationComponent],
 })
