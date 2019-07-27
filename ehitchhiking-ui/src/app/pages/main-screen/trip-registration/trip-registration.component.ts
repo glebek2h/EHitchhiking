8654f078a934478598b65c345257c76ecc1f0303
Gleb <raul221299@gmail.com>
2019-07-27 16:54:14 +0300

    cleaning form with validators 😔

--- ehitchhiking-ui/src/app/pages/main-screen/trip-registration/trip-registration.component.ts
+++ ehitchhiking-ui/src/app/pages/main-screen/trip-registration/trip-registration.component.ts
@@ -1,14 +1,14 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {User} from "@shared/models/user";
-import {UserState} from "@shared/enums/UserState";
+import {User} from '@shared/models/user';
+import {UserState} from '@shared/enums/UserState';
 
 @Component({
 	selector: 'app-trip-registration',
 	templateUrl: './trip-registration.component.html',
 	styleUrls: ['./trip-registration.component.sass'],
 })
-export class TripRegistrationComponent implements OnInit {
+export class TripRegistrationComponent implements OnInit, OnChanges {
 	@Input() isShown: boolean;
   @Input() user: User;
   @Input() userState: UserState;
@@ -17,6 +17,7 @@ export class TripRegistrationComponent implements OnInit {
 	@Output() isShownSaveRouteButton = new EventEmitter<boolean>();
 
 	nameFormGroup: FormGroup;
+  isInit: boolean;
 
 	constructor() {}
 
@@ -29,7 +30,29 @@ export class TripRegistrationComponent implements OnInit {
       departureTime: new FormControl('', [Validators.required]),
       car: new FormControl(''),
 		});
-	}
+    this.isInit = true;
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.userState && this.isInit) {
+
+      this.nameFormGroup.controls.from.reset();
+      this.nameFormGroup.controls.to.reset();
+      this.nameFormGroup.controls.departureDate.reset();
+      this.nameFormGroup.controls.placesSelect.reset();
+      this.nameFormGroup.controls.departureTime.reset();
+      this.nameFormGroup.controls.from.setErrors(null);
+      this.nameFormGroup.controls.to.setErrors(null);
+      this.nameFormGroup.controls.departureDate.setErrors(null);
+      this.nameFormGroup.controls.placesSelect.setErrors(null);
+      this.nameFormGroup.controls.departureTime.setErrors(null);
+      /*	this.nameFormGroup.controls.from.updateValueAndValidity();
+        this.nameFormGroup.controls.to.updateValueAndValidity();
+        this.nameFormGroup.controls.departureDate.updateValueAndValidity();
+        this.nameFormGroup.controls.placesSelect.updateValueAndValidity();
+        this.nameFormGroup.controls.departureTime.updateValueAndValidity();*/
+    }
+  }
 
 	onChangeFix(event: Event, target) {
 		const input = event.target as HTMLInputElement;
