3f5ea997f3e9f815f31b3b518f9a0360694ff69e
Gleb <raul221299@gmail.com>
2019-07-30 13:25:13 +0300

    refactoring

--- ehitchhiking-ui/src/app/shared/components/rate-passengers-modal/rate-passengers-modal.component.ts
+++ ehitchhiking-ui/src/app/shared/components/rate-passengers-modal/rate-passengers-modal.component.ts
@@ -1,4 +1,4 @@
-import {Component, Inject} from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import {MatDialogRef} from '@angular/material';
 import {StarClickMeta} from '../rating/starClickMeta';
 import {MAT_DIALOG_DATA} from '@angular/material';
@@ -10,31 +10,36 @@ import {UserState} from "@shared/enums/UserState";
 	templateUrl: './rate-passengers-modal.component.html',
 	styleUrls: ['./rate-passengers-modal.component.sass'],
 })
-export class RatePassengersModalComponent {
-	passengers: User[] = [
-    {id: 0, name: 'Ivan', rating: 0, isAddedToBlackList: false},
-    {id: 1, name: 'Egor', rating: 0, isAddedToBlackList: false},
-    {id: 2, name: 'Semen', rating: 0, isAddedToBlackList: false},
-    {id: 3, name: 'Fedor', rating: 0, isAddedToBlackList: false},
-	];
-  driver: User = {id: 0, name: 'Ivan', rating: 0, isAddedToBlackList: false};
+export class RatePassengersModalComponent implements OnInit{
+  users: User[] = [];
   UserState = UserState;
 
   constructor(public dialogRef: MatDialogRef<RatePassengersModalComponent>, @Inject(MAT_DIALOG_DATA) public data: any) {
   }
 
-	rateUser(clickObj: StarClickMeta): void {
-		const passenger = this.passengers.find((i: User) => i.id === clickObj.itemId);
-		if (!!passenger) {
-			passenger.rating = clickObj.rating;
+  ngOnInit(): void {
+    // here should be just GET 'info' about trip
+    if (this.data.dataKey === UserState.Driver){
+      this.users = [
+        {id: 0, name: 'Ivan', rating: 0, isAddedToBlackList: false},
+        {id: 1, name: 'Egor', rating: 0, isAddedToBlackList: false},
+        {id: 2, name: 'Semen', rating: 0, isAddedToBlackList: false},
+        {id: 3, name: 'Fedor', rating: 0, isAddedToBlackList: false},
+      ];
+    }
+    else{
+      this.users = [{id: 0, name: 'Ivan', rating: 0, isAddedToBlackList: false}];
+    }
+
+
+  }
+
+  rateUser(clickObj: StarClickMeta): void {
+		const user = this.users.find((i: User) => i.id === clickObj.itemId);
+		if (!!user) {
+			user.rating = clickObj.rating;
 		}
 	}
-
-  rateDriver(clickObj: StarClickMeta) {
-    if (!!this.driver) {
-      this.driver.rating = clickObj.rating;
-    }
-  }
 
 	exitTrip(): void {
 		this.dialogRef.close();
@@ -46,12 +51,9 @@ export class RatePassengersModalComponent {
 
   addToBlacklist(i) {
     console.log(this.data);
-    this.passengers[i].isAddedToBlackList = !this.passengers[i].isAddedToBlackList;
+    this.users[i].isAddedToBlackList = !this.users[i].isAddedToBlackList;
   }
 
-  addDriverToBlacklist() {
-    this.driver.isAddedToBlackList = !this.driver.isAddedToBlackList;
-  }
 
   exit() {
     this.dialogRef.close();
