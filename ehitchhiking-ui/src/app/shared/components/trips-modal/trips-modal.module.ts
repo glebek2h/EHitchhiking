7279aed4c6b4753ef3cb8bf3c86a7e3aea65757e
Julia <youliaja.romanova@gmail.com>
2019-08-01 12:25:06 +0300

    fixbug

--- ehitchhiking-ui/src/app/shared/components/trips-modal/trips-modal.module.ts
+++ ehitchhiking-ui/src/app/shared/components/trips-modal/trips-modal.module.ts
@@ -15,15 +15,11 @@ import {TripModule} from '../trip/trip.module';
 import {NoDataModule} from '../no-data/no-data.module';
 import {PreLoadingModule} from '../pre-loading/pre-loading.module';
 import {TripsModalComponent} from './trips-modal.component';
-import {SortTripsPipe} from '../../pipes/sort-trips.pipe';
-import {FilterByRolePipe} from '../../pipes/filter-by-role.pipe';
-import {FormsModule} from '@angular/forms';
 import {FilterByFavoritePipe} from '../../pipes/filter-by-favorite.pipe';
 import {RatePassengersModalModule} from "@shared/components/rate-passengers-modal/rate-passengers-modal.module";
 import {SortTripsPipe} from '../../pipes/sort-trips.pipe';
 import {FilterByRolePipe} from '../../pipes/filter-by-role.pipe';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {FilterByFavoritePipe} from '../../pipes/filter-by-favorite.pipe';
 import {FilterByStatusPipe} from '../../pipes/filter-by-status.pipe';
 import {FilterByRatingPipe} from '@shared/pipes/filter-by-rating.pipe';
 
