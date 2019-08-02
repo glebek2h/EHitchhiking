471fca66000294e5a373fa8e6393b81535543fc1
Julia <youliaja.romanova@gmail.com>
2019-08-02 11:50:06 +0300

    removed excess import and changed style

--- ehitchhiking-ui/src/app/shared/components/active-trips-modal/active-trips-modal.module.ts
+++ ehitchhiking-ui/src/app/shared/components/active-trips-modal/active-trips-modal.module.ts
@@ -11,20 +11,13 @@ import {
 	MatSlideToggleModule,
 	MatTableModule,
 } from '@angular/material';
-import {TripModule} from '../trip/trip.module';
 import {NoDataModule} from '../no-data/no-data.module';
 import {PreLoadingModule} from '../pre-loading/pre-loading.module';
-import {SortTripsPipe} from '../../pipes/sort-trips.pipe';
-import {FilterByRolePipe} from '../../pipes/filter-by-role.pipe';
 import {FormsModule} from '@angular/forms';
-import {FilterByFavoritePipe} from '../../pipes/filter-by-favorite.pipe';
 import {ActiveTripsModalComponent} from './active-trips-modal.component';
-import {ActiveTripsModalService} from './active-trips-modal.service';
-import {AppModule} from '../../../app.module';
 import {ActiveTripModule} from '../active-trip/active-trip.module';
 import {TripsModalModule} from '../trips-modal/trips-modal.module';
 import {ActiveTripAdditionalInfoModule} from '../active-trip-additional-info/active-trip-additional-info.module';
-import { ConfirmationModalModule } from "@shared/modals/confirmation-modal/confirmation-modal.module";
 
 @NgModule({
 	declarations: [ActiveTripsModalComponent],
