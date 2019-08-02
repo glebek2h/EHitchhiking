cf070a264d279ec8ecc110d4f40fbc480e0b7b37
Gleb <raul221299@gmail.com>
2019-08-02 17:26:27 +0300

    increase rate passsengers(drivers) modal width

--- ehitchhiking-ui/src/app/shared/components/trip/trip.component.ts
+++ ehitchhiking-ui/src/app/shared/components/trip/trip.component.ts
@@ -3,7 +3,10 @@ import {Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
 import {StarClickMeta} from '../rating/starClickMeta';
 import {MatDialog} from '@angular/material';
 import {RatePassengersModalComponent} from '@shared/components/rate-passengers-modal/rate-passengers-modal.component';
-import {DEFAULT_MAT_DIALOG_CLASS, MAT_DIALOG_WIDTH_SM} from '@shared/constants/modal-constants';
+import {
+  DEFAULT_MAT_DIALOG_CLASS,
+  MAT_DIALOG_WIDTH_SMD
+} from '@shared/constants/modal-constants';
 import {UserState} from '@shared/enums/UserState';
 import {TripHistory} from '@shared/interfaces/trip-history-interface';
 @Component({
@@ -47,7 +50,7 @@ export class TripComponent implements OnInit {
 
 	openRatePassengersDialog() {
 		this.dialog.open(RatePassengersModalComponent, {
-			width: MAT_DIALOG_WIDTH_SM,
+			width: MAT_DIALOG_WIDTH_SMD,
 			panelClass: DEFAULT_MAT_DIALOG_CLASS,
 			autoFocus: false,
 			data: {
--- ehitchhiking-ui/src/app/shared/constants/modal-constants.ts
+++ ehitchhiking-ui/src/app/shared/constants/modal-constants.ts
@@ -2,6 +2,7 @@ export const DEFAULT_MAT_DIALOG_CLASS = 'mat-dialog-no-padding';
 export const DEFAULT_MAT_SNACK_CLASS = 'notification-container';
 export const MAT_SNACK_HORIZONTAL_POSITION = 'right';
 export const MAT_DIALOG_WIDTH_SM = '23rem';
+export const MAT_DIALOG_WIDTH_SMD = '32rem';
 export const MAT_DIALOG_WIDTH_MD = '40rem';
 export const MAT_DIALOG_WIDTH_LG = '55rem';
 export const DELETE_ROUTE_MARKER = -1;
