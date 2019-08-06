831e33ae247700c738e353d29ff10a93991b6e74
Julia <youliaja.romanova@gmail.com>
2019-08-06 12:02:35 +0300

    refresh

--- ehitchhiking-ui/src/app/shared/components/active-trip/active-trip.component.ts
+++ ehitchhiking-ui/src/app/shared/components/active-trip/active-trip.component.ts
@@ -1,13 +1,11 @@
 import {Component, Input, OnInit, SimpleChanges} from '@angular/core';
 import { EventEmitter, Output} from '@angular/core';
-import {ActiveTrip} from './active-trip';
 import {UserState} from '@shared/enums/UserState';
 import {DEFAULT_MAT_DIALOG_CLASS, MAT_DIALOG_WIDTH_SM} from '@shared/constants/modal-constants';
 import {MatDialog} from '@angular/material';
 import {ConfirmationModalComponent} from '@shared/modals/confirmation-modal/confirmation-modal.component';
 import {ActiveTrip} from '@shared/models/active-trip';
 import {ActiveTripsApiService} from '@shared/services/api.services/active-trips.api.service';
-import {LoaderSize} from '@shared/enums/pre-loader-sizes';
 
 @Component({
 	selector: 'app-active-trip',
@@ -15,7 +13,7 @@ import {LoaderSize} from '@shared/enums/pre-loader-sizes';
 	styleUrls: ['./active-trip.component.sass'],
 })
 export class ActiveTripComponent implements OnInit {
-	constructor(public dialog: MatDialog, private activeTripsApiService: ActiveTripsApiService) {}
+	constructor(public dialog: MatDialog) {}
 	userState = UserState;
 
 	@Input() trip: ActiveTrip;
