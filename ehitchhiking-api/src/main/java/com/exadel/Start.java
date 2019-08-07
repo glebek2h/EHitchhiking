4b3f270350f4b067c78d5335c2e75ea9237142a2
Chernyakov <alexey.chernyakov00@gmail.com>
2019-08-07 12:45:55 +0300

    tmp commit

--- ehitchhiking-api/src/main/java/com/exadel/Start.java
+++ ehitchhiking-api/src/main/java/com/exadel/Start.java
@@ -1,11 +1,15 @@
 package com.exadel;
 
+import com.exadel.ehitchhiking.services.IAutoService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication(exclude = {
         DataSourceAutoConfiguration.class,
         DataSourceTransactionManagerAutoConfiguration.class,
@@ -15,4 +19,5 @@ public class Start {
     public static void main(String[] args) {
         SpringApplication.run(Start.class, args);
     }
+
 }
